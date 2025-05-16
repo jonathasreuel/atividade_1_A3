1c567f08621a165b53905cf0e2b3e2699b2162fd
Jonathas Reuel <jowrsl@gmail.com>
2025-05-16 20:44:57 -0300

    chore: Ajustes no index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const produtosRoutes = require('./routes/produtos');
+const clientesRoutes = require('./routes/clientes');
 
 app.use(express.json());
 app.use('/produtos', produtosRoutes);
+app.use('/clientes', clientesRoutes);
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
-
